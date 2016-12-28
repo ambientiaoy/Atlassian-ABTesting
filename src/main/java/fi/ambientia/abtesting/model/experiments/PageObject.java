cfb3beb4e3e63851c793497251eaafb1eee470f3
Aki Salmi <aki.salmi@iki.fi>
2016-12-28 15:12:38 +0200

    Taking space keys from properties, now

---
+++ src/main/java/fi/ambientia/abtesting/model/experiments/PageObject.java
@@ -0,0 +1,13 @@
+package fi.ambientia.abtesting.model.experiments;
+
+public class PageObject {
+    private final String page;
+
+    public PageObject(String page) {
+        this.page = page;
+    }
+
+    public String getPage() {
+        return page;
+    }
+}
